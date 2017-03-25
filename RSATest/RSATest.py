c99c8290cc2daccd68370a0ac69de3abb69c2ae4
Milacikovia <milacikovia@hmamail.com>
2017-03-25 12:24:45 +0100

    Add files via upload

---
+++ RSATest/RSATest.py
@@ -0,0 +1,23 @@
+﻿import rsa
+from base64 import b64encode, b64decode
+from Crypto import Random
+
+# Message will be a secret key for AES
+msg1 = "Hello teamMates, I am the key!"
+#rand = Random.get_random_bytes(8)
+
+#msg2 = "Hello teamMates, I am okay!"
+keysize = 2048
+(public, private) = rsa.newkeys(keysize)
+encrypted = b64encode(rsa.encrypt(msg1.encode(), private))
+decrypted = rsa.decrypt(b64decode(encrypted), private)
+signature = b64encode(rsa.sign(msg1.encode(), private))
+verify = rsa.verify(msg1.encode(), b64decode(signature), public)
+
+print(private.exportKey('PEM'))
+print(public.exportKey('PEM'))
+print("Encrypted: " + encrypted.decode())
+print("Decrypted: '%s'" % decrypted)
+print("Signature: " + signature.decode())
+print("Verify: %s" % verify)
+#rsa.verify(msg2.encode(), b64decode(signature), public)
